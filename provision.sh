bcccf82302fc3f795017a42875a8f91d6f148bef
james@jamespamplin.com <james@jamespamplin.com>
2014-09-25 22:15:52 +0000

    Boot to GNOME

--- provision.sh
+++ provision.sh
@@ -8,5 +8,8 @@ yum install -y git vim zsh docker-io screen
 # NFS Exports
 echo "/home/vagrant  192.168.19.0/255.255.255.0(rw,sync,no_subtree_check,insecure,all_squash,anonuid=`id -u`,anongid=`id -g`)" >> /etc/exports
 
+# Boot straight to GNOME shell
+ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+
 # Change shell to zsh
 chsh -s `which zsh` vagrant
