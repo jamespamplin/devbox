4856c1bba665d373dbf8292c1b8b3c41a43f55dc
James Pamplin <james.pamplin@guardian.co.uk>
2015-01-06 11:53:15 +0000

    Added infinality font config + adobe source code pro font

--- provision.sh
+++ provision.sh
@@ -5,7 +5,13 @@ yum groupinstall -y "GNOME Desktop"
 yum install -y git vim zsh docker-io screen
 
 # Install gui tools
-yum install -y gimp inkscape
+yum install -y gimp inkscape adobe-source-code-pro-fonts
+
+# Better Font rendering with infinality: http://www.infinality.net/blog/infinality-freetype-patches/
+rpm -Uvh http://www.infinality.net/fedora/linux/infinality-repo-1.0-1.noarch.rpm
+yum install -y freetype-infinality fontconfig-infinality
+
+/etc/fonts/infinality/infctl.sh setstyle osx2
 
 # NFS Exports - exposes home dir to host
 echo "/home/vagrant  192.168.19.0/255.255.255.0(rw,sync,no_subtree_check,insecure,all_squash,anonuid=`id -u vagrant`,anongid=`id -g vagrant`)" >> /etc/exports
